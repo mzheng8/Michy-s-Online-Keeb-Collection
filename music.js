408be990e834f15d7dae7d3af52569ef3c42c306
Michy <56746370+michymichelle@users.noreply.github.com>
2022-05-29 12:46:37 -0400

    Add files via upload

---
+++ music.js
@@ -0,0 +1,5 @@
+const playmusicbutton = document.querySelector('.musicbutton');
+let summerlight = document.querySelector('#audio');
+playmusicbutton.addEventListener('click',()=>{
+    summerlight.play();
+});
